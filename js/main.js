4d84f8bd4ab0eb4e1c9670c83a9ce21e87eeed0f
misbahedm <misbah129@gmail.com>
2019-04-03 12:57:26 +0600

    add running class

--- js/main.js
+++ js/main.js
@@ -2,10 +2,14 @@
 
 //Credits to ChessMaster for helping me with this code ^_^
 
-onload = function() {
-    var date = new Date(); 
+//mohified by misbahedm & jamilxt 
+
+onload = function () {
+
+    var date = new Date();
     var current = date.getHours() * 60 + date.getMinutes();
     var time;
+
     if (current >= 9 * 60 + 01 && current < 10 * 60 + 05)
         time = "001";
     else if (current >= 10 * 60 + 05 && current < 10 * 60 + 55)
@@ -20,31 +24,54 @@ onload = function() {
         time = "006";
     else if (current >= 14 * 60 + 15 && current < 15 * 60 + 05)
         time = "007";
-    
-    
-    
-   // else if (current >= 24 * 60 + 20 && current < 24 * 60 + 50)
+
+
+
+    // else if (current >= 24 * 60 + 20 && current < 24 * 60 + 50)
     //    time = "008";
-        
-        
-        
-            
-    if (!time)
+
+
+
+
+    if (!time) {
         return;
+    }
+
     var today = date.getDay(); // 3
-    
+
+
+
     var children = document.getElementById(time).childNodes;
-    for (var i = 0; i < children.length; i++)  {
+
+    for (var i = 0; i < children.length; i++) {
+
         var classes = children[i].classList;
-        if (!classes)
+
+        if (!classes) {
+
             continue;
-        for (var j = 0; j < classes.length; j++)  {
-            if (classes[j] == "d" + today) {
-                var h = document.getElementById("highlight");
-                if (h) h.setAttribute("id", "");
-                children[i].setAttribute("id", "highlight");
-            }
+
         }
+
+
+
+
+        for (var j = 0; j < classes.length; j++) {
+
+            if (classes[j] == "d" + today) {
+
+                var highlight_id = document.getElementById("highlight");
+
+                if (highlight_id) {
+                    highlight_id.setAttribute("id", "");
+                }
+
+                children[i].setAttribute("id", "highlight");
+
+            }
+
+        }
+
     }
 
 
@@ -55,7 +82,7 @@ onload = function() {
 
     showDate();
     showTime();
-
+    running_class();
 
 
 
@@ -71,48 +98,66 @@ onload = function() {
 //clock script
 
 
-function showTime(){
+function showTime() {
     var date_clock = new Date();
     var h = date_clock.getHours(); // 0 - 23
     var m = date_clock.getMinutes(); // 0 - 59
     var s = date_clock.getSeconds(); // 0 - 59
     var session = "AM";
-    
-    if(h == 0){
-      h = 12;
+
+    if (h == 0) {
+        h = 12;
     }
-    
-    if(h > 12){
-      h = h - 12;
-      session = "PM";
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
     }
-    
+
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
-    
+
     var time_clock = h + ":" + m + ":" + s + " " + session;
-    
+
     document.getElementById("MyClockDisplay").innerText = time_clock;
-    
+
     setTimeout(showTime, 1000);
-    
+
+}
+
+
+
+//bangla date fucntion  
+function showDate() {
+
+    var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    var date_div = new Date();
+
+    date_div = date_div.toLocaleDateString("bn-BD", options);
+
+
+    document.getElementById("MyDateDisplay").innerText = date_div;
+
+
+
+
+
+}
+
+//runnging class function
+function running_class() {
+
+
+    var highlighted_text = document.getElementById("highlight").innerText;
+
+    var running_class_div = document.getElementById("running_class");
+
+    if (highlighted_text != "") {
+        running_class_div.innerText = "এখন চলছে \"" + highlighted_text + "\" ক্লাস";
+    } else {
+        running_class_div.innerText = "এখন ক্লাস বন্ধ";
     }
-    
-    
-    function showDate(){
-
-        var options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
-        var date_div = new Date();
-
-        date_div = date_div.toLocaleDateString("bn-BD", options); 
 
 
-        document.getElementById("MyDateDisplay").innerText = date_div;
-
-
-
-
-
-    }
-    
+}
