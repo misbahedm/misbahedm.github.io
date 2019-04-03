12add8e92b2cf3e6c3da1f932bd511fd3adb4b1e
misbahedm <misbah129@gmail.com>
2019-04-03 10:56:11 +0600

    fix html dayclass according to javascript

--- js/main.js
+++ js/main.js
@@ -23,15 +23,16 @@ onload = function() {
     
     
     
-    else if (current >= 24 * 60 + 20 && current < 24 * 60 + 50)
-        time = "008";
+   // else if (current >= 24 * 60 + 20 && current < 24 * 60 + 50)
+    //    time = "008";
         
         
         
             
     if (!time)
         return;
-    var today = date.getDay(); 
+    var today = date.getDay(); // 3
+    console.log(today); 
     var children = document.getElementById(time).childNodes;
     for (var i = 0; i < children.length; i++)  {
         var classes = children[i].classList;
@@ -50,33 +51,4 @@ onload = function() {
 
 //clock script
 
-function showTime(){
-var date = new Date();
-var h = date.getHours(); // 0 - 23
-var m = date.getMinutes(); // 0 - 59
-var s = date.getSeconds(); // 0 - 59
-var session = "AM";
-
-if(h == 0){
-  h = 12;
-}
-
-if(h > 12){
-  h = h - 12;
-  session = "PM";
-}
-
-h = (h < 10) ? "0" + h : h;
-m = (m < 10) ? "0" + m : m;
-s = (s < 10) ? "0" + s : s;
-
-var time = h + ":" + m + ":" + s + " " + session;
-document.getElementById("MyClockDisplay").innerText = time;
-document.getElementById("MyClockDisplay").textContent = time;
-
-setTimeout(showTime, 1000);
-
-}
-
-showTime();
 
